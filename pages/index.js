a1c2bab0bd1103d4432c5863bb3af5bc67ad0397
manlalaro1 <odonnellp46@gmail.com>
2021-02-11 22:29:49 -0500

    edited schema, index.js

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,46 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import Hero from '../components/Hero'
+import useSWR from 'swr';
+import { gql } from 'graphql-request';
+import Layout from '../components/layout';
+import styles from '../styles/Home.module.css';
+import { graphQLClient } from '../utils/graphql-client';
 
-const Guestbook = (props) => {
+const fetcher = async (query) => await graphQLClient.request(query);
+
+const Home = () => {
+   const { data, error } = useSWR(
+    gql`
+      {
+        allTodos {
+          data {
+            _id
+            task
+            completed
+          }
+        }
+      }
+    `,
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <link
-          rel="shortcut icon"
-          type="image/x-icon"
-          href="/static/favicon.png"
-        />
-      </Head>
-      <style jsx global>{`
-        body {
-          margin: 0px;
-          padding: 0px;
-        }
-      `}</style>
-      <div>
-        <Hero />
-        <Footer />
-      </div>
-      <style jsx>{`
-        div {
-          display: flex;
-          margin-left: auto;
-          margin-right: auto;
-          font-family: sans-serif, sans;
-          flex-direction: column;
-          align-items: center;
-        }
-      `}</style>
-    </>
-  )
-}
+    <Layout>
+      <h1>Next Fauna GraphQL CRUD</h1>
 
-export default Guestbook
+      {data ? (
+        <ul>
+          {data.allTodos.data.map((todo) => (
+            <li key={todo._id} className={styles.todo}>
+              <span>{todo.task}</span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>loading...</div>
+      )}
+    </Layout>
+  );
+};
+
+export default Home;
